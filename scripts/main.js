9168f0fa443260ff719515ed8992dde5e904fb9d
Kirill <efremenkov1999@gmail.com>
2021-08-27 00:28:54 +0300

    add calc

--- scripts/main.js
+++ scripts/main.js
@@ -263,4 +263,52 @@ window.addEventListener('DOMContentLoaded', function () {
 
     addDots();
     slider();
+
+    // calculator
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block');
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcDay = document.querySelector('.calc-day');
+        const calcCount = document.querySelector('.calc-count');
+        const totalValue = document.getElementById('total');
+
+        const countSome = () => {
+            let total = 0;
+            let countValue = 1;
+            let dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+
+            if(calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if(calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
+
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        }
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            if(target.matches('.calc-type') ||
+            target.matches('.calc-square')||
+            target.matches('.calc-day')||
+            target.matches('.calc-count')){
+                countSome();
+            }
+        });
+    };  
+
+    calc(100);
 });
