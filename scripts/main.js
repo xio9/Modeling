d758d46247f6f455f2d0b31be1c369970f0aa340
Kirill <efremenkov1999@gmail.com>
2021-09-02 22:26:31 +0300

    less26

--- scripts/main.js
+++ scripts/main.js
@@ -349,4 +349,55 @@ window.addEventListener('DOMContentLoaded', function () {
     }
 
     form();
+
+    const sendForm = () => {
+        const errorMessage = 'Что-то пошло не так...';
+        const loadMessage = 'Загрузка';
+        const successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+        const form = document.getElementById('form1');
+
+        const statusMessage = document.createElement('div');
+        statusMessage.textContent = 'Тут будет сообщение!';
+        statusMessage.style.cssText = 'font-size: 2rem;'
+        
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            const request = new XMLHttpRequest();
+
+            request.addEventListener('readystatechange', () => {
+                statusMessage.textContent = loadMessage;
+
+                if(request.readyState !== 4){
+                    return;
+                }
+
+                if(request.status === 200){
+                    statusMessage.textContent = successMessage;
+                } else {
+                    statusMessage.textContent = errorMessage;
+                }
+            });
+
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'application/json');
+            
+            const formData = new FormData(form);
+            let body = {};
+
+            // for(let val of formData.entries()){
+            //     body[val[0]] = val[1];
+            // }
+
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+
+            request.send(JSON.stringify(body));
+        });
+    };
+
+    sendForm();
 });
